912d869693bae14b87e148d30be4c0e5dec2a0f2
koliveiras <koliveiras@indracompany.com>
2022-02-21 20:44:12 -0300

    commit dos imports

--- src/main/java/br/com/nexusapp/api/exception/ServiceUnavailableException.java
+++ src/main/java/br/com/nexusapp/api/exception/ServiceUnavailableException.java
@@ -2,6 +2,11 @@ package br.com.nexusapp.api.exception;
 
 public class ServiceUnavailableException extends RuntimeException{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5734544386026469718L;
+
 	public ServiceUnavailableException(String msg){
         super(msg);
     }
